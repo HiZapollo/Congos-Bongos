cfbbd5b0cb90e977b3ca0d47f5925668d56d683c
CSillus <71670415+csills1416@users.noreply.github.com>
2023-10-26 18:47:21 -0400

    Created Signup Page

---
+++ develop/client/src/pages/Signup.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            alert('Passwords do not match!');
+            return;
+        }
+        // Handle signup logic here (e.g., call an API to register the user)
+    }
+
+    return (
+        <div>
+            <h1>Signup</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Username: </label>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password: </label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Confirm Password: </label>
+                    <input
+                        type="password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Signup</button>
+            </form>
+        </div>
+    );
+}
+
+export default Signup;
