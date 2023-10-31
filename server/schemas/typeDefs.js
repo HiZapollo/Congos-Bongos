ece5d5aed08d43c94156fe59bd1bde75df7fac91
Shabadoo9 <brinstonzbpe@gmail.com>
2023-10-30 20:03:28 -0400

    added query and mutation to typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -58,14 +58,21 @@ const typeDefs = `
 
         type Query {
             me: User
+            categories: [Type] # Add a query to retrieve all categories
+            Bongos(types: [ID], name: String): [Bongo] # Query to fetch Bongos based on type or name
+            Bongo(_id: ID): Bongo # Query to get a single Bongo by ID
+            order(_id: ID): Order # Query to retrieve a single Order by ID
         }
-
+    
         type Mutation {
             login(email: String!, password: String!): Auth
             addUser(username: String!, email: String!, password: String!): Auth
             saveBongo(input: BongoInput!): User
-            removeBongo(bongoId: String!): User
+            removeBongo(bongoId: ID!): User
+            addOrder(Bongos: [ID]!): Order # Mutation to add an Order based on Bongo IDs
+            updateUser(username: String, email: String): User # Mutation to update user details
+            updateBongo(_id: ID!, quantity: Int): Bongo # Mutation to update Bongo quantity
         }
-`;
+    `;
 
 module.exports = typeDefs;
