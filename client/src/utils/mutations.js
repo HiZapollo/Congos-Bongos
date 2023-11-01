997847e393e4769613f3d4d89e473790c985b291
CSillus <71670415+csills1416@users.noreply.github.com>
2023-11-01 15:19:37 -0400

    changed queries.js and mutations.js so it would match the backend

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,65 +1,84 @@
 import { gql } from '@apollo/client';
 
-export const MUTATION_ADD_BONGO = gql`
-  mutation addBongo($input: BongoInput!) {
-    addBongo(input: $input) {
-      id
-      name
-      description
+export const LOGIN_USER = gql`
+    mutation LoginUser($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
     }
-  }
 `;
 
-export const MUTATION_UPDATE_BONGO = gql`
-  mutation updateBongo($id: ID!, $input: BongoInput!) {
-    updateBongo(id: $id, input: $input) {
-      id
-      name
-      description
+export const ADD_USER = gql`
+    mutation AddUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
     }
-  }
 `;
 
-export const MUTATION_DELETE_BONGO = gql`
-  mutation deleteBongo($id: ID!) {
-    deleteBongo(id: $id) {
-      id
+export const SAVE_BONGO = gql`
+    mutation SaveBongo($input: BongoInput!) {
+        saveBongo(input: $input) {
+            _id
+            username
+            savedBongos {
+                _id
+                name
+            }
+        }
     }
-  }
 `;
 
-export const MUTATION_SIGNUP_USER = gql`
-  mutation signup($input: SignupInput!) {
-    signup(input: $input) {
-      token
-      user {
-        id
-        username
-      }
+export const REMOVE_BONGO = gql`
+    mutation RemoveBongo($bongoId: ID!) {
+        removeBongo(bongoId: $bongoId) {
+            _id
+            username
+            savedBongos {
+                _id
+                name
+            }
+        }
     }
-  }
 `;
 
-export const MUTATION_LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        id
-        username
-      }
+export const ADD_ORDER = gql`
+    mutation AddOrder($Bongos: [ID]!) {
+        addOrder(Bongos: $Bongos) {
+            _id
+            purchaseDate
+            Bongos {
+                _id
+                name
+            }
+        }
     }
-  }
 `;
 
-export const MUTATION_ADD_ORDER = gql`
-  mutation addOrder($bongos: [ID!]!) {
-    addOrder(bongos: $bongos) {
-      purchaseDate
-      Bongos {
-        id
-        name
-      }
+export const UPDATE_USER = gql`
+    mutation UpdateUser($username: String, $email: String) {
+        updateUser(username: $username, email: $email) {
+            _id
+            username
+            email
+        }
     }
-  }
+`;
+
+export const UPDATE_BONGO = gql`
+    mutation UpdateBongo($_id: ID!, $quantity: Int) {
+        updateBongo(_id: $_id, quantity: $quantity) {
+            _id
+            name
+            quantity
+        }
+    }
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,69 +1,76 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_ALL_BONGOS = gql`
-  query allBongos {
-    bongos {
-      id
-      name
-      description
-      image
-      quantity
-      price
-      types {
-        id
-        name
-      }
-    }
-  }
-`;
-
-export const QUERY_BONGO = gql`
-  query bongo($id: ID!) {
-    bongo(id: $id) {
-      id
-      name
-      description
-      image
-      quantity
-      price
-      types {
-        id
-        name
-      }
-    }
-  }
-`;
-
-export const QUERY_USER = gql`
-  query user($id: ID!) {
-    user(id: $id) {
-      id
-      username
-      email
-      savedBongos {
-        purchaseDate
-        Bongos {
-          id
-          name
+export const GET_TYPES = gql`
+    query GetTypes {
+        types {
+            _id
+            name
         }
-      }
     }
-  }
 `;
 
-export const QUERY_ME = gql`
-  query me {
-    me {
-      id
-      username
-      email
-      savedBongos {
-        purchaseDate
-        Bongos {
-          id
-          name
+export const GET_BONGOS_BY_TYPE_OR_NAME = gql`
+    query GetBongos($types: [ID], $name: String) {
+        bongos(types: $types, name: $name) {
+            _id
+            name
+            description
+            image
+            quantity
+            price
+            types {
+                _id
+                name
+            }
         }
-      }
     }
-  }
+`;
+
+export const GET_SINGLE_BONGO = gql`
+    query GetSingleBongo($id: ID!) {
+        bongo(_id: $id) {
+            _id
+            name
+            description
+            image
+            quantity
+            price
+            types {
+                _id
+                name
+            }
+        }
+    }
+`;
+
+export const GET_SINGLE_ORDER = gql`
+    query GetSingleOrder($id: ID!) {
+        order(_id: $id) {
+            _id
+            purchaseDate
+            Bongos {
+                _id
+                name
+                description
+            }
+        }
+    }
+`;
+
+export const GET_SINGLE_USER = gql`
+    query GetSingleUser($id: ID!) {
+        user(_id: $id) {
+            _id
+            username
+            email
+        }
+    }
+`;
+
+export const INIT_CHECKOUT = gql`
+    query InitCheckout($bongos: [BongoInput]) {
+        checkout(bongos: $bongos) {
+            session
+        }
+    }
 `;
