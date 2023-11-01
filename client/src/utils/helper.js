5aaaed8b880dbe3c72de40e2ed81818a5880d3a8
CSillus <71670415+csills1416@users.noreply.github.com>
2023-11-01 12:10:08 -0400

    uncommented the helper.js code, added error handling, provided data types.

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,44 +1,66 @@
-// // Formats price to a standard currency display (e.g., $100.00)
-// export const formatPrice = (price) => {
-//     return `$${parseFloat(price).toFixed(2)}`;
-// };
+// Formats price to a standard currency display (e.g., $100.00)
+export const formatPrice = (price) => {
+    if (typeof price !== 'number' || isNaN(price)) {
+        console.error("Invalid price value:", price);
+        return null;
+    }
+    return `$${parseFloat(price).toFixed(2)}`;
+};
 
-// // Truncate long product descriptions for display in product listings
-// export const truncateDescription = (description, maxLength = 100) => {
-//     if (description.length <= maxLength) return description;
-//     return `${description.substring(0, maxLength)}...`;
-// };
+// Truncate long product descriptions for display in product listings
+export const truncateDescription = (description, maxLength = 100) => {
+    if (typeof description !== 'string') {
+        console.error("Invalid description value:", description);
+        return "";
+    }
+    if (description.length <= maxLength) return description;
+    return `${description.substring(0, maxLength)}...`;
+};
 
-// // Calculate total price for items in cart
-// export const calculateTotal = (cartItems) => {
-//     return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
-// };
+// Calculate total price for items in cart
+export const calculateTotal = (cartItems) => {
+    if (!Array.isArray(cartItems)) {
+        console.error("Invalid cartItems value:", cartItems);
+        return 0;
+    }
+    return cartItems.reduce((total, item) => {
+        if (typeof item.price !== 'number' || typeof item.quantity !== 'number') {
+            console.error("Invalid item in cart:", item);
+            return total;
+        }
+        return total + item.price * item.quantity;
+    }, 0);
+};
 
-// // Generate a unique ID (useful for items that don't have an ID yet, e.g., newly added to cart)
-// export const generateUniqueId = () => {
-//     return '_' + Math.random().toString(36).substr(2, 9);
-// };
+// Generate a unique ID (useful for items that don't have an ID yet, e.g., newly added to cart)
+export const generateUniqueId = () => {
+    return '_' + Math.random().toString(36).substr(2, 9);
+};
 
-// // Check if product is on sale
-// export const isOnSale = (product) => {
-//     return product.salePrice && product.salePrice < product.regularPrice;
-// };
+// Check if product is on sale
+export const isOnSale = (product) => {
+    if (!product || typeof product.salePrice !== 'number' || typeof product.regularPrice !== 'number') {
+        console.error("Invalid product:", product);
+        return false;
+    }
+    return product.salePrice && product.salePrice < product.regularPrice;
+};
 
-// // Calculate the discounted price
-// export const calculateDiscountedPrice = (product) => {
-//     if (isOnSale(product)) {
-//         return product.salePrice;
-//     }
-//     return product.regularPrice;
-// };
+// Calculate the discounted price
+export const calculateDiscountedPrice = (product) => {
+    if (isOnSale(product)) {
+        return product.salePrice;
+    }
+    return product.regularPrice;
+};
 
-// // Convert Congo sizes to a display label (e.g., S => Small)
-// export const sizeToLabel = (size) => {
-//     const sizeMap = {
-//         'S': 'Small',
-//         'M': 'Medium',
-//         'L': 'Large',
-//         'XL': 'Extra Large'
-//     };
-//     return sizeMap[size] || size;
-// };
+// Convert Congo sizes to a display label (e.g., S => Small)
+export const sizeToLabel = (size) => {
+    const sizeMap = {
+        'S': 'Small',
+        'M': 'Medium',
+        'L': 'Large',
+        'XL': 'Extra Large'
+    };
+    return sizeMap[size] || size;
+};
