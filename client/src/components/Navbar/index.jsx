e183b715bc59b59a2566937911a0f6cb355844c3
Shabadoo9 <brinstonzbpe@gmail.com>
2023-11-02 14:21:49 -0400

    linked aboutus to navbar

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -9,6 +9,11 @@ function Nav() {
         if (Auth.loggedIn()) {
             return (
                 <ul className="flex-row">
+                    <li>
+                        <Link to="/about">
+                            About Us
+                        </Link>
+                    </li>
                     <li className="mx-1">
                         {/* this is not using the Link component to logout or user and then refresh the application to the start */}
                         <a href="/" onClick={() => Auth.logout()}>
