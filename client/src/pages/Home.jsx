c2d8220a309926dcbdd51eebad90d45344bf023a
zeckenrode <zeckenrode9000@gmail.com>
2023-11-02 02:58:17 -0400

    Added standard home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,29 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+import Cart from "../components/Cart";
 
 const Home = () => {
-    const [bongos, setBongos] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost:5000/bongos')
-            .then(response => response.json())
-            .then(data => setBongos(data));
-    }, []);
-
-    return (
-        <div>
-            <h1>Welcome to Bongo Amazon</h1>
-            <div className="bongo-grid">
-                {bongos.map(bongo => (
-                    <div key={bongo._id} className="bongo-card">
-                        <img src={bongo.imageUrl} alt={bongo.name} />
-                        <h2>{bongo.name}</h2>
-                        <p>{bongo.description}</p>
-                        <p>Price: ${bongo.price}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="container">
+      <CategoryMenu />
+      <ProductList />
+      <Cart />
+    </div>
+  );
+};
 
 export default Home;
